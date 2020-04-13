7157d566eba02eec29380bf61b7ffb401380e01f
Alexander Davydov <aleksandrd@wix.com>
2020-04-13 09:19:04 +0300

    Initial commit from Create Yoshi App

---
+++ src/components/__mocks__/i18n.ts
@@ -0,0 +1,17 @@
+import i18next from 'i18next';
+
+// We initialize i18next once with configuration for component tests.
+void i18next.init({
+  // This configuration causes i18n to always return the key from calling `t('...')`
+  fallbackLng: 'cimode',
+
+  // Change to `true` to see more information
+  debug: false,
+
+  // Configure our translate HOC to not wait for data to be available
+  react: {
+    wait: false,
+  },
+});
+
+export default i18next;
